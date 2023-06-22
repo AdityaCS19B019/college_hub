88eec98bc724289a902da18f9af2e97205d13d2b
AdityaCS19B019 <cs19b019@iittp.ac.in>
2023-06-22 14:29:57 +0530

    version1

--- server.js
+++ server.js
@@ -8,21 +8,25 @@ const authrouter = require('./routes/auth')
 const postsrouter = require('./routes/posts')
 const commentsrouter = require('./routes/comments')
 app.listen(5000 , () => console.log('api started'))
-app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 // app.use(errorHandler);
 url = process.env.DATABASE_URL
 console.log(url)
 mongoose.set('strictQuery', false)
-mongoose.connect(url , {useNewUrlParser : true})
-// app.use(cors())
+app.use(express.json())
+try{
+    mongoose.connect(url , {useNewUrlParser : true})
+}catch(e){
+    console.log("error at connecting to dataase")
+}
+app.use(cors())
 // const db = mongoose.connection
 
 // db.on('error' , (error) => console.error(error))
 
 // db.once('open' , () => console.log('connected to db'))
 app.get('/' , (req,res) => {
-    res.send("Hellp bro")
+    res.send("Hello bro")
 })
 app.use('/auth' , authrouter)
 app.use('/post' , postsrouter)
